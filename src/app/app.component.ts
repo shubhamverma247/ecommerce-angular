9baa566425bc5a32cb46a77597fc44417609775d
Shubham Verma <vermashubham286@gmail.com>
2024-04-04 20:24:45 +0530

    add edit delete user for admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/layouts/footer/footer.component';
 import { HeaderComponent } from './shared/layouts/header/header.component';
@@ -12,5 +12,19 @@ import { HeaderComponent } from './shared/layouts/header/header.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ecommerce';
+  screenHeight: any;
+  screenWidth: any;
+  footerMaxHeight!: number;
+  title = 'angularecommerce';
+
+  constructor() {
+    this.getScreenSize(event);
+  }
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event: any) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    //console.log(this.screenHeight, this.screenWidth)
+    this.footerMaxHeight = this.screenHeight - 160;
+  }
 }
